528d4b1b4ce485cebadc745f2970cd68f45547cf
MiguelVasquezR <mvasquesrosas@gmail.com>
2024-12-18 10:36:54 -0600

    aplicación de CORS

--- src/java/ws/ApplicationConfig.java
+++ src/java/ws/ApplicationConfig.java
@@ -29,6 +29,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(ws.CorsFilter.class);
         resources.add(ws.WSCliente.class);
         resources.add(ws.WSColaborador.class);
         resources.add(ws.WSEnvios.class);
